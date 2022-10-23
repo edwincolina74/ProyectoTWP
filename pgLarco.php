dbcab718bd415dc895e8768af8b7f6acd1316e94
edwin <edwincolina74@gmail.com>
2022-10-23 01:28:50 -0500

    agradando los php

--- pgLarco.php
+++ pgLarco.php
@@ -10,21 +10,9 @@
 </head>
 <body>
 	<!-- CABECERA -->
-	<header class="cabecera">
-		<a href="index.html"><img src="imagenes/inicio/logohotel.png"></a>
-		<nav class="navegacion">
-			<a href="index.html">HOTEL IMPERIAL</a>
-			<a href="habitaciones.html" >Habitaciones</a>
-			<a href="ofertas.html">Paquetes y Ofertas</a>
-			<a href="eventos.html">Eventos</a>
-			<a href="bodas.html">Bodas</a>
-			<a href="servicios.html">Servicios</a>
-			<a href="atracciones.html">Atracciones locales</a>
-			<a href="contactoyubicacion.html">Contáctanos</a>
-			<a href="ejercicios.html" target="contenido">Ejercicios</a> 
-		</nav>
-		<a href="index.html"><img src="imagenes/inicio/logohotel.png"></a>
-	</header>
+	<?php
+	include "Intro/cabecera.php"
+	?>
 	<!-- FIN CABECERA    -->
 	<div class="enlaces">
 		<p><a href="atracciones.html">Atracciones Turísticas</a> >&nbsp;&nbsp;MUSEO LARCO</p>
@@ -77,49 +65,10 @@
 	<br><br>
 
 	<!-- PIE DE PAGINA -->
+	<?php
+	include "Intro/piepagina.php"
+	?>
+	<!-- FIN: PIE DE PAGINA -->
 
-		<footer >
-			<div class="contenedor-pie-pagina1">
-				<div class="pie-columna1">
-					<a href="index.html"><h1>HOTEL IMPERIAL</h1></a> 
-					<p>Calle los claveles 155, Lima, Perú</p>
-					<p>(511) 285-3015</p>
-					<p>consulta@hotelimperial.com.pe</p>
-				</div>
-				<form class="pie-subscribete">
-					<label>SUBSCRIBETE</label>
-					<input type="email" name="nombre" placeholder="Ingresar correo electrónico">
-					<input type="submit" name="aceptar" value="REGISTRATE">
-				</form>
-			</div>
-
-			<div class="contenedor-pie-pagina2" >
-				<div>
-					<h2><a href="contactoyubicacion.html">CONTACTO Y UBICACION </a></h2>
-				</div>
-				<div >
-					<h2><a href="miembrospreferentes.html">MIEMBROS PREFERENTES </a></h2>
-				</div>
-				<div class="pie-columna2">		
-					<div>		
-						<h1>SIGUENOS </h1>
-					</div>
-					<div class="iconos-tamano">
-						<p>
-							<a href="https://web.facebook.com/Hoteles.com/about/?ref=page_internal&_rdc=1&_rdr"><img src="imagenes/inicio/facebook.png">
-								<a href="https://twitter.com/riuhoteles" ><img src="imagenes/inicio/twitter.png">
-									<a href="https://www.instagram.com/hotelsdotcom/"><img src="imagenes/inicio/instagram.png">
-										<a href="https://www.youtube.com/channel/UCrpymEUtWUYzLoK-fGG8uXA"><img src="imagenes/inicio/youtube.png">
-										</p>
-									</div>
-								</div>
-								<div>
-									<h2><a href="libro.html"> Libro de Reclamaciones </a></h2>
-								</div>
-							</div>
-						</footer>
-
-						<!-- FIN: PIE DE PAGINA -->
-
-					</body>
-					</html>
+</body>
+</html>
