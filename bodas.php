cbc3f1120b0b7f267984e7e1e8345f2f5790f3b1
Nancyqm <114556276+Nancyqm@users.noreply.github.com>
2022-10-23 22:08:59 -0500

    Update bodas.php
    cambiado cabecera y pie

--- bodas.php
+++ bodas.php
@@ -86,52 +86,9 @@
 	<hr>
 <br>
 
-<!-- PIE DE PAGINA -->
-
-		<footer >
-			<div class="contenedor-pie-pagina1">
-				<div class="pie-columna1">
-					<a href="index.html"><h1>HOTEL IMPERIAL</h1></a> 
-					<p>Calle los claveles 155, Lima, Perú</p>
-					<p>(511) 285-3015</p>
-					<p>consulta@hotelimperial.com.pe</p>
-				</div>
-				<form class="pie-subscribete">
-					<label>SUBSCRIBETE</label>
-					<input type="email" name="nombre" placeholder="Ingresar correo electrónico">
-					<input type="submit" name="aceptar" value="REGISTRATE">
-				</form>
-			</div>
-
-			<div class="contenedor-pie-pagina2" >
-				<div>
-					<h2><a href="contactoyubicacion.html">CONTACTO Y UBICACION </a></h2>
-				</div>
-				<div >
-					<h2><a href="miembrospreferentes.html">MIEMBROS PREFERENTES </a></h2>
-				</div>
-				<div class="pie-columna2">		
-					<div>		
-						<h2>SIGUENOS </h2>
-					</div>
-					<div class="iconos-tamano">
-						<p>
-							<a href="https://web.facebook.com/Hoteles.com/about/?ref=page_internal&_rdc=1&_rdr"><img src="imagenes/inicio/facebook.png">
-								<a href="https://twitter.com/riuhoteles" ><img src="imagenes/inicio/twitter.png">
-									<a href="https://www.instagram.com/hotelsdotcom/"><img src="imagenes/inicio/instagram.png">
-										<a href="https://www.youtube.com/channel/UCrpymEUtWUYzLoK-fGG8uXA"><img src="imagenes/inicio/youtube.png">
-										</p>
-									</div>
-								</div>
-								<div>
-									<h2><a href="libro.html"> LIBRO DE RECLAMACIONES </a></h2>
-								</div>
-							</div>
-						</footer>
-
-						<!-- FIN: PIE DE PAGINA -->
-
-
+<?php
+	include "Intro/piepagina.php"
+?>
 
 </body>
 </html>
