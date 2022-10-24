18e636215cf48cc31ba3a07d4dea563da569317c
Eliza1Trujillo <114605388+Eliza1Trujillo@users.noreply.github.com>
2022-10-24 00:10:59 -0500

    Add files via upload

---
+++ eventos.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EVENTOS</title>
+	<link rel="stylesheet" type="text/css" href="css/eventos.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos1.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<!-- CABECERA --> 
+	<?php
+	include "Intro/cabecera.php"
+	?>
+	<!-- FIN CABECERA    -->
+	<div><h1 class="centrar">Eventos</h1></div>
+	<div><p>OFRECEMOS SALAS PARA REUNIONES Y/O EVENTOS</p></div>
+	<h2 class="centrar2">Servicios</h2>
+	<div class="flex-col">   
+		<section id="cajapadre">
+			<div>
+				<ul>
+					<li>Wifi</li>
+					<li>Aire acondicionado</li>
+					<li>Equipos audivisuales</li>
+					<li>Seguridad 24 horas</li>
+				</ul>
+			</div>
+			<div>
+				<ul>
+					<li>Seguridad 24 horas</li>
+					<li>Videoconferencias</li>
+					<li>Arreglos florales</li>
+					<li>Videoconferencias</li>
+				</ul>
+			</div>
+			<div>
+				<ul>
+					<li>Bar</li>
+					<li>Menu perzonalizado</li>
+					<li>Decoracion a corde al evento</li>
+				</ul>
+			</div>
+		</section>
+	</div>
+	<a href="planificacion.php"><input type="button" value="PLANIFICAR"></a>
+	<br><br>
+	<hr>
+	<div><h2 class="centrar2">Salas para Reuniones</h2></div>
+	<div><p>Nuestro hotel es una de las primeras opcines para las personas por nuestras amplias salas para gran cantidad de personas o para darle comodidad al cliente,</p>
+		<p>ademas tambien contamos con pequeñas salas, algunas de estas bien reforzadas para que no tengan interrupciones, ya sea de bulla u otras cosas que icomoden al cliente</p></div>
+		<div class="tabla">
+			<div class="item-ofertas item">
+				<h3>Juntas directivas</h3>
+				<img src="imagenes/eventos/juntadirectiva.jpg">
+			</div>
+			<div class="item-ofertas item">
+				<h3>Exposiciones</h3>
+				<img src="imagenes/eventos/Exposiciones.jpg">
+			</div>
+		</div>
+		<div class="tabla">
+			<div class="item-ofertas item">
+				<h3>Sala Small Directory</h3>
+				<img src="imagenes/eventos/small.jpg">
+			</div>
+			<div class="item-ofertas item">
+				<h3>Reunion de Colegas</h3>
+				<img src="imagenes/eventos/colegas.jpg">
+			</div>
+		</div>
+		<br>
+		<hr>
+		<div><h2 class="centrar2">Salas para Eventos</h2></div>
+		<div><p>Ademas de ofrecer salas para reunines, nuestro hotel cuenta con slas con diversos tipos de acabados, pero si el cliente lo desea</p>
+			<p>esos acabados se pueden modificar ya que contamos con una amplia tecnologia que nos ayuda a brindarle al cliente una buena experiencia</p></div>
+			<div class="tabla">
+				<div class="item-ofertas item2">
+					<h3>Pequeña</h3>
+					<img src="imagenes/eventos/small.jpg">
+				</div>
+				<div class="item-ofertas item2">
+					<h3>Mediana</h3>
+					<img src="imagenes/eventos/mediano.jpg">
+				</div>
+				<div class="item-ofertas item2">
+					<h3>Grande</h3>
+					<img src="imagenes/eventos/grande.jpg">
+				</div>
+			</div>
+			<br><br><br>	
+
+			<!-- PIE DE PAGINA -->
+
+			<?php
+			include "Intro/piepagina.php"
+			?>
+			<!-- FIN: PIE DE PAGINA -->
+
+		</body>
+		</html>
