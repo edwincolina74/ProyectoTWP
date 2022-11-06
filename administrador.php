0ecf82dd5ad83de0c03aa077e0eb2cbba34f64b0
Julio Benigno Galindo Acuna <jgalindoa@unmsm.edu.pe>
2022-11-06 02:03:59 -0500

    NADA

--- administrador.php
+++ administrador.php
@@ -4,8 +4,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+
+
+
 </head>
 <body>
+
 
 		<h1>Gestion de Tipo de Habitaciones</h1>
 		<a href="paginas/thabitacion/agregar.php">Agregar tipo de habitación</a><br>
@@ -34,6 +43,8 @@
 		<h1>Gestion de Reservaciones</h1>
 		<a href="paginas/producto/agregar.php">Agregar Producto</a><br>
 		<a href="paginas/producto/listar.php">Listar Producto</a><br><br>
+
+
 
 </body>
 </html>
