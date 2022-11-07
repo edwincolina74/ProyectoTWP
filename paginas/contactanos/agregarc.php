34a20b5d4a5011b8cc5d1713ed6151e4737f4b39
Nancyqm <114556276+Nancyqm@users.noreply.github.com>
2022-11-06 22:20:10 -0500

    Add files via upload

---
+++ paginas/contactanos/agregarc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="../../llamadas/proceso_contactanos.php" method="post">
+		<label>Nombre</label>
+		<input type="text" name="nomb"><br>
+		<label>Apellido</label>
+		<input type="text" name="apell"><br>
+		<label>Teléfono</label>
+		<input type="int" name="telef"><br>
+		<label>email</label>
+		<input type="text" name="email"><br>
+		<label>Comentario</label>
+		<input type="text" name="comentario"><br>
+		<input type="submit" name="" value="Aceptar">
+	</form>
+
+</body>
+</html>
