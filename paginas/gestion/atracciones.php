54f60cd3c6ef20189fc4fee633e68ef1805f85d8
Nancyqm <114556276+Nancyqm@users.noreply.github.com>
2022-12-11 16:33:17 -0500

    Update atracciones.php
    se modifico por el incluide de cabecera y pie

--- paginas/gestion/atracciones.php
+++ paginas/gestion/atracciones.php
@@ -17,19 +17,9 @@
 <body>
 
 	<!-- CABECERA! -->
-	<header class="cabecera">
-		<div><a href="index.php"><img src="../../Imagenes/inicio/logohotel.png"></a> </div>
-		<div class="cabeceracolorletra"><a href="../../index.php">HOTEL IMPERIAL</a> </div>
-		<div class="cabeceracolorletra"> <a href="habitaciones.php" >Habitaciones</a> </div>
-		<div class="cabeceracolorletra"><a href="ofertas.php">Paquetes y Ofertas</a> </div>
-		<div class="cabeceracolorletra"><a href="eventos.php">Eventos</a></div>	
-		<div class="cabeceracolorletra"><a href="bodas.php">Bodas</a> </div>
-		<div class="cabeceracolorletra"><a href="servicios.php">Servicios</a></div>
-		<div class="cabeceracolorletra"><a href="atracciones.php">Atracciones locales</a></div>
-		<div class="cabeceracolorletra"><a href="contactoyubicacion.php">Contactanos</a></div> 
-		<div class="cabeceracolorletra"><a href="bak/administrador.php" target="contenido">ADMINISTRADOR</a></div> 
-		<div><a href="index.php"><img src="../../Imagenes/inicio/logohotel.png"></a> </div>
-	</header>
+	<?php
+		include "../../Intro/cabecera.php"
+	?>
 	<!-- FIN CABECERA -->
 	
 	<section>
@@ -147,53 +137,10 @@
 		width="800" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 	</div>
 	<!--PIE DE PAGINA-->
-
-	<footer class="footer">
-			<div class="container">
-				<div class="row">
-					<div class="footer-col">
-						<h4>HOTEL IMPERIAL</h4>
-						<ul>
-							<li> <p>Calle los claveles 155, Lima, Perú </p>	</li>
-							<li><p>(511) 285-3015</p></li>
-							<li> <p>consulta@hotelimperial.com.pe</p></li>
-
-						</ul>
-					</div>
-					<div class="footer-col">
-						<h4>INFORMACION</h4>
-						<ul>
-							<li><a href="contactoyubicacion.php">Contacto y ubicación</a></li>
-							<li><a href="libro.php">libro de reclamaciones</a></li>
-
-						</ul>    
-					</div>
-					<div class="footer-col">
-						<h4>REGISTRATE</h4>
-						<ul>
-							<li><a href="miembrospreferentes.php">miembros preferentes</a></li>
-							
-								<form class="pie-subscribete">
-									<label>SUBSCRIBETE</label>
-									<input type="email" name="nombre" placeholder="Ingresar correo electrónico">
-									<input type="submit" name="aceptar" value="REGISTRATE">
-								</form>
-						</ul> 
-					</div>
-					<div class="footer-col">
-						<h4>SIGUENOS</h4>
-						<div class="social-links iconos-tamano">
-
-						<a href="https://web.facebook.com/Hoteles.com/about/?ref=page_internal&_rdc=1&_rdr"> <img src="../../imagenes/inicio/facebook.png"></a>
-						<a href="https://twitter.com/riuhoteles" ><img src="../../imagenes/inicio/twitter.png"></a>
-							<a href="https://www.instagram.com/hotelsdotcom/"><img src="../../imagenes/inicio/instagram.png"></a>
-							<a href="https://www.youtube.com/channel/UCrpymEUtWUYzLoK-fGG8uXA"><img src="../../imagenes/inicio/youtube.png"></a>
-
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!-- FIN: PIE DE PAGINA -->
+	<?php
+	         include "../../Intro/piepagina.php"
+        ?>
+	
+	<!-- FIN: PIE DE PAGINA -->
 </body>
 </html>
