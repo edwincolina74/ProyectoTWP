7fdfb208f6b9038306b5aca8bae91fc4df478da5
Eliza1Trujillo <114605388+Eliza1Trujillo@users.noreply.github.com>
2022-12-12 09:26:43 -0500

    Add files via upload

---
+++ llamadas/proceso_reclamo.php
@@ -0,0 +1,27 @@
+<?php
+	require('../controlador/conexionreclamo.php');
+	$conn = conectar();
+
+	$action= $_REQUEST['accion'];
+	if($action=="agregar"){
+		$name = $_REQUEST['nombre'];
+		$tdoc = $_REQUEST['tipoDoc'];
+		$dis= $_REQUEST['dist']; 
+		$dni= $_REQUEST['DNI'];
+		$dir= $_REQUEST['direc'];
+		$tel= $_REQUEST['fono'];
+		$correo= $_REQUEST['email'];
+
+		agregarDatos($name,$tdoc,$dis,$dni,$dir,$tel,$correo, $conn);
+		$codre = $dni;
+
+		$npersonas=$_REQUEST['ngente'];
+		$ta=$_REQUEST['tipoA'];
+		$tp=$_REQUEST['tipo'];
+		$mount=$_REQUEST['monto'];
+		$msg=$_REQUEST['desc'];
+		agregarReclamo($codre,$npersonas,$ta,$tp,$mount,$msg,$conn);
+	}
+	header('location:../paginas/reclamo/listar.php');
+
+?>
