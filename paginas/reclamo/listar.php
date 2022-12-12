65e183eef8f0d25bc07098cbbf578c0c5819167f
Nancyqm <114556276+Nancyqm@users.noreply.github.com>
2022-12-12 02:38:49 -0500

    Add files via upload

--- paginas/reclamo/listar.php
+++ paginas/reclamo/listar.php
@@ -3,12 +3,20 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="../../css/tablasbd.css">
+	<link rel="stylesheet" type="text/css" href="../../css/estilos1.css">
+		<link rel="stylesheet" href="../../css/style.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
 	<?php
-	include "../Intro/cabecera.php"
+	include "../../Intro/cabecera.php";
+	require('../../controlador/conexionreclamo.php');
+		$conn = conectar();
 	?>
-		<h2>Listar reclamos</h2>
+		<h2>Listar Reclamos</h2>
 
 <div>
 	<table>
@@ -38,9 +46,7 @@
 </div>
 
 	<!-- PIE DE PAGINA -->
-	<?php
-	include "../Intro/piepagina.php"
-	?>
+	
 
 	<!-- FIN: PIE DE PAGINA -->
 </body>
